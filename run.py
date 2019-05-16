0a480d80ea8ead51705e60c2f8039a96055bb16f
BahrmaLe <akuksenko@gmail.com>
2019-05-16 12:05:34 +0300

    Added run script with argparse parameters

--- run.py
+++ run.py
@@ -4,17 +4,21 @@ import subprocess
 a = "C:\\Users\\60064265\\PycharmProjects\\Homework\\API\\tests.py"
 b = "C:\\Users\\60064265\\PycharmProjects\\Homework\\OpenBreweryDB\\tests.py"
 c = "C:\\Users\\60064265\\PycharmProjects\\Homework\\CDNJS\\tests.py"
-
+# d = "C:\\Users\\60064265\\GitHub\\otus-qa\\Lesson3\\example1-parametrize\\test_example1_params.py"
+names = [a, b, c]
 
 parser = argparse.ArgumentParser()
-# parser.add_argument("-p", "--path", action="store", dest="command")
 parser.add_argument('--dogs', action='store_const', const=a, dest="command")
 parser.add_argument('--opn', action='store_const', const=b, dest="command")
 parser.add_argument('--cdn', action='store_const', const=c, dest="command")
+# parser.add_argument('--wrong', action='store_const', const=d, dest="command")
+# parser.add_argument("-p", "--path", action="store", dest="command")
 
 args = parser.parse_args()
-# print(args.command)
-# This way argument can be manipulated.
+
+if args.command not in names:
+    raise argparse.ArgumentTypeError("is not allowed directory")
+
 if args.command == a:
     print("Dogs API testing")
 elif args.command == b:
