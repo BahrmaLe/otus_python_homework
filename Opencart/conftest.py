db76440b9231a747fc6b125aafba1404b8f888b7
BahrmaLe <akuksenko@gmail.com>
2019-05-17 12:53:36 +0300

    Added run script with argparse parameters

---
+++ Opencart/conftest.py
@@ -0,0 +1,40 @@
+import sys
+import pytest
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions, FirefoxOptions, IeOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption("--address", action="store", default="http://192.168.56.103/", help="Opencart web address")
+    parser.addoption("--browser", action="store", default="firefox", help="Browser name")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def driver(request):
+    browser = request.config.getoption("--browser")
+    if browser == 'firefox':
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        capabilities = options.to_capabilities()
+        wd = webdriver.Firefox(desired_capabilities=capabilities)
+        wd.maximize_window()
+    elif browser == 'chrome':
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        capabilities = options.to_capabilities()
+        wd = webdriver.Chrome(desired_capabilities=capabilities)
+        wd.fullscreen_window()
+    elif browser == 'ie':
+        options = IeOptions()
+        options.add_argument("--headless")
+        capabilities = options.to_capabilities()
+        wd = webdriver.Ie(desired_capabilities=capabilities)
+        wd.fullscreen_window()
+    else:
+        print('Unsupported browser!')
+        sys.exit(1)
+    yield wd
+    wd.quit()
+
+
+
---
+++ Opencart/tests.py
@@ -0,0 +1,7 @@
+def test_open_home_page(driver, request):
+    url = 'opencart/'
+    return driver.get("".join([request.config.getoption("--address"), url]))
+    element = driver.find_elements_by_xpath("//base[contains(@href, 'http://192.168.56.103/opencart/]")
+    assert element == 'http://192.168.56.103/opencart/'
+    assert driver.find_element_by_class_name('col-sm-4').text == 'Your Store'
+
