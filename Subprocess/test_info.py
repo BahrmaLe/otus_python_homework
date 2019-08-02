c8b047ff5b841363c220d0cb7e7f846c578bcb0f
BahrmaLe <akuksenko@gmail.com>
2019-08-02 14:24:22 +0300

    Added run script with argparse parameters

--- Subprocess/test_info.py
+++ Subprocess/test_info.py
@@ -44,7 +44,7 @@ def test_if_stat():
     print(resp)
     pat = re.compile(r'enp0s8: ([1-9]\d*)', re.MULTILINE)
     print(pat)
-    enp = pat.findall(resp)[0]
+    enp = pat.findall(resp)
     print(enp)
     # capturedproc = []
     # try:
