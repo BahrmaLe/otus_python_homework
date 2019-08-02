7a63ea8aaf8a37b0962f558fb0dd85b6bf9c0434
BahrmaLe <akuksenko@gmail.com>
2019-08-02 16:31:49 +0300

    Added run script with argparse parameters

--- Subprocess/test_info.py
+++ Subprocess/test_info.py
@@ -1,8 +1,24 @@
 import logging
 import re
 import subprocess
+import argparse
+from time import sleep
 
 logging.basicConfig(level=logging.INFO)
+
+
+def args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-p', action='store', dest='package', default="bash",
+                        help='Get version of the package')
+    parser.add_argument('-d', action='store', dest='dir', default=".",
+                        help='Get file list in the directory')
+    parser.add_argument('--port', action='store', dest='port', default=":21",
+                        help='Get file list in the directory')
+    parser.add_argument('--program', action='store', dest='program', default="",
+                        help='Get program info')
+    return parser.parse_args()
 
 
 def test_ifconfig():
@@ -75,3 +91,56 @@ def test_os_version():
     resp = subprocess.check_output(["cat", "/proc/version"]).decode()
     assert "Ubuntu 7.4.0-1ubuntu1~18.04.1)" in resp
     logging.info(resp)
+
+
+if __name__ == "__main__":
+    # Check version of the package
+    arguments = args()
+    if args() == "package":
+        print("Getting package version")
+        logging.info("".join(['package =' + arguments.package]))
+        logging.info("getting verion info")
+        p = subprocess.Popen([arguments.package, "--version"])
+        p.communicate()
+        sleep(2)
+
+    if args() == "dir":
+        print("Getting files in the current directory")
+        # Get files in the current directory
+        logging.info(arguments.dir)
+        logging.info("getting files list")
+        resp = subprocess.check_output(["ls", "-l", arguments.dir]).decode()
+        print(resp)
+        sleep(2)
+
+    if args() == "port":
+        print("Getting port activity")
+        logging.info(arguments.port)
+        """getting port state"""
+        p1 = subprocess.Popen(['netstat', '-atnp'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        p2 = subprocess.Popen(["grep", arguments.port], stdin=p1.stdout, stdout=subprocess.PIPE)
+        line = p2.stdout.readline()
+        print(line.decode())
+        sleep(2)
+
+    if args() == "program":
+        print("Getting info about process")
+        # Get info about process
+        logging.info("".join(["programm ", arguments.program]))
+        p1 = subprocess.Popen(["ps", "aux"],
+                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        p2 = subprocess.Popen(["grep", arguments.program], stdin=p1.stdout, stdout=subprocess.PIPE)
+        line = p2.stdout.read()
+        print(line.decode())
+
+
+#     print(subprocess.check_output(["ps", "aux"]).decode())
+#
+#     if args() == "package":
+#         print("Getting package version")
+#     if args()  == "dir":
+#         print("openbreweryDB API testing")
+# elif args.command == c:
+#     print("CDNJS API testing")
+# results = (subprocess.check_output(["pytest", "-v", args.command]))
+# print(results.decode('utf-8'))
