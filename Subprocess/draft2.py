66c29a1c773e3cda4f92ecc74482666f9214d823
BahrmaLe <akuksenko@gmail.com>
2019-08-08 17:45:27 +0300

    Added run script with argparse parameters

--- Subprocess/draft2.py
+++ Subprocess/draft2.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
         raise SystemExit
     elif arguments.apache:
         print(arguments.apache)
-        resp = subprocess.check_output(["pytest", "-s", "-v", "draft2.py::" + "test_apache_stat"])
+        resp = subprocess.check_output(["pytest", "-s", "-v", "draft2.py::" + "test_apache_stat"], shell=True).decode()
         print(resp)
         raise SystemExit
     # elif arguments.p:
